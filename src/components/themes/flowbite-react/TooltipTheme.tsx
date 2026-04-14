9f61a43a077e35be2a0ec4ff504497b446f518d7
John <edwardlomat1503@gmail.com>
2026-04-14 16:02:31 +0800

    Remove unused flowbite-react theme files
    ButtonTheme and TooltipTheme used CustomFlowbiteTheme which was
    removed in flowbite-react 0.12.x. Themes are now inlined directly
    on components.

--- src/components/themes/flowbite-react/TooltipTheme.tsx
+++
@@ -1,9 +0,0 @@
-import type { CustomFlowbiteTheme } from 'flowbite-react'
-
-const TooltipTheme: CustomFlowbiteTheme = {
-  tooltip: {
-    content: 'relative z-20 whitespace-nowrap',
-  },
-}
-
-export default TooltipTheme
